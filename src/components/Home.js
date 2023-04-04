0799e1f997d36c6d8bd983105e6f06e45c2c9fa4
Andrew <n10768009@qut.edu.au>
2023-04-04 19:59:14 +1000

    updated the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/css/Home.css';
+import '../assets/css/utilities.css';
 
 const Home = () => {
   return (
     <div className="home">
       <main>
         <section id="hero">
-          <h1>Welcome to My Portfolio</h1>
-          <p>I'm a software engineer with a passion for creating amazing things.</p>
-          <Link to="about"><button>About</button></Link>
-          <Link to="contact"><button>Contact</button></Link>
+          <h2>G'day! I'm</h2>
+          <h1>Andrew Wilks</h1>
+          <p>An Aussie computer science student venturing through the digital Outback,<br/>Wrangling code as a full-stack developer in the making.</p>
+          {/* <Link to="about"><button>About</button></Link> */}
+          {/* <Link to="contact"><button>Contact</button></Link> */}
+          
+          <div className='scroll-down'><span>scroll down <i className="gg-arrow-down-r" style={{"--ggs": 0.6}}/></span></div>
+          <div className='scroll-down scroll-down--left'><span>scroll down <i className="gg-arrow-down-r" style={{"--ggs": 0.6}}/></span></div>
         </section>
       </main>
     </div>

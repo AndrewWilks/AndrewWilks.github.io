5a767b363005008241abc73c6c1d95200d63867a
Andrew <n10768009@qut.edu.au>
2023-04-05 19:15:52 +1000

    readded the routes to the correct pages

--- src/App.js
+++ src/App.js
@@ -4,23 +4,26 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Resume from './components/Resume';
 import Contact from './components/Contact';
-import UnderConstruction from './components/UnderConstruction';
-import NotFound from './components/notFound.js';
+import Blog from './components/Blog.js';
 
+// import UnderConstruction from './components/UnderConstruction';
+import NotFound from './components/notFound.js';
+import NavBar from './utils/utils';
 
 function App() {
   return (
-      <div>
-        {/* Add your navigation menu here */}
-        <Routes>
-          <Route path="" exact element={<Home/>} />
-          <Route path="about" element={<UnderConstruction/>} />
-          <Route path="projects" element={<UnderConstruction/>} />
-          <Route path="resume" element={<UnderConstruction/>} />
-          <Route path="contact" element={<UnderConstruction/>} />
-          <Route path="*"element={<NotFound/>} />
-        </Routes>
-      </div>
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="" exact element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="resume" element={<Resume />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="blog" element={<Blog />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
   );
 }
 
